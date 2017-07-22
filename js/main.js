3443399153fa88cb0deb53cd9e2864bdc42526b4
Nickolay Krumov <nickkrumov@gmail.com>
2017-07-22 17:04:19 +0300

    drawing skills

--- js/main.js
+++ js/main.js
@@ -1,24 +1,61 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
 
-canvas.width = 934;
-canvas.height = 622;
+var Drawer = {};
+Drawer.bgCanvas = document.getElementById("background-canvas");
+Drawer.wCanvas = document.getElementById("window-canvas");
+Drawer.bgCtx = Drawer.bgCanvas.getContext("2d");
+Drawer.wCtx = Drawer.wCanvas.getContext("2d");
+Drawer.pixelSize = 10;
 
+Drawer.bgCanvas.width = 270;
+Drawer.bgCanvas.height = 330;
+Drawer.wCanvas.width = 270;
+Drawer.wCanvas.height = 330;
 
-var background = new Image();
-background.src = "images/charlie_grayscale.jpg";
-background.crossOrigin="anonymous";
+Drawer.drawBg = function() {
+	var bgImg = new Image();
+	bgImg.src = "images/charlie_grayscale.jpg";
+	bgImg.crossOrigin="anonymous";
 
-background.onload = function(){
-    ctx.drawImage(background,0,0);   
-	var pixelData = ctx.getImageData(166, 44, 1, 1).data;
-	console.log(pixelData);
+	bgImg.onload = function(){
+		Drawer.bgCtx.drawImage(bgImg,0,0);   
+	}
 }
 
-var canvas2 = document.getElementById("canvas2"),
-    ctx2 = canvas2.getContext("2d");
+Drawer.drawWindow = function(x, y) {
+	let s = this.pixelSize;
+	let drawX = x-s;
+	this.wCtx.beginPath();
+	for (let i = 0; i < 3; i++, drawX += s) {
+		let drawY = y-s;	
+		for (let z = 0; z < 3; z++, drawY += s) {
+			if(drawX < 0 || drawX > this.wCanvas.width) {
+				continue;
+			}
+			if(drawY < 0 || drawY > this.wCanvas.height) {
+				continue;
+			}
+			this.wCtx.rect(drawX,drawY,s,s);
+		}
+	}
+	this.wCtx.closePath();
+	this.wCtx.strokeStyle="#FF0000";
+	this.wCtx.stroke();
+}
 
-canvas2.width = 300;
-canvas2.height = 300;
-ctx2.fillStyle = "#FF0000";
-ctx2.fillRect(0,0,22,22);
+Drawer.moveWindow = function(x, y) {
+	this.wCtx.clearRect(0, 0, this.wCanvas.width, this.wCanvas.height);
+	this.drawWindow(x, y);
+}
+
+Drawer.exampleBg = function() {
+	
+}
+
+Drawer.drawBg();
+Drawer.drawWindow(60, 40);
+setTimeout(function() {
+	Drawer.moveWindow(111, 55);
+}, 1000);
+setTimeout(function() {
+	Drawer.moveWindow(1, 55);
+}, 2000);
